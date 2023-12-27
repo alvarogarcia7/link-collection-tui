c8d28de026b8d173b26bd5295d5171888d8d548c
Alvaro Garcia <alvarogarcia7@users.noreply.github.com>
2023-12-27 23:56:28 +0400

    Copy the example from https://github.com/bczsalba/pytermgui/blob/master/utils/readme_scripts/contact.py + convert to an Widget

--- main.py
+++ main.py
@@ -1,16 +1,84 @@
-import time
+from typing import Any
 
 import pytermgui as ptg
+from pytermgui import Widget
+from pytermgui.pretty import pprint
 
-def macro_time(fmt: str) -> str:
-    return time.strftime(fmt)
+OUTPUT = {}
 
-if __name__ == '__main__':
 
-    ptg.tim.define("!time", macro_time)
+class EditorWidget(Widget):
 
-    with ptg.WindowManager() as manager:
-        manager.layout.add_slot("Body")
-        manager.add(
-            ptg.Window("[bold]The current time is:[/]\n\n[!time 75]%c", box="EMPTY")
-        )
+    def handle_key(self, key: str) -> bool:
+        print(f"Received key: {key}")
+
+    def __init__(self, **attrs: Any):
+        super().__init__(**attrs)
+        with ptg.WindowManager() as manager:
+            window = (ptg.Window(
+                "",
+                ptg.InputField("Balazs", prompt="Name: "),
+                ptg.InputField("Some street", prompt="Address: "),
+                ptg.InputField("+11 0 123 456", prompt="Phone number: "),
+                "",
+                ptg.Container(
+                    "Additional notes:",
+                    ptg.InputField(
+                        "A whole bunch of\nMeaningful notes\nand stuff", multiline=True,
+                    ),
+                    box="EMPTY_VERTICAL",
+                ),
+                "",
+                ["Submit", lambda *_: self.submit(manager, window)],
+                width=60,
+                box="DOUBLE",
+            )
+                      .set_title("[210 bold]New contact")
+                      .center()
+                      )
+
+            # For the screenshot's sake
+            window.select(0)
+
+            manager.add(window)
+
+    def submit(self, manager: ptg.WindowManager, window: ptg.Window) -> None:
+        for widget in window:
+            if isinstance(widget, ptg.InputField):
+                OUTPUT[widget.prompt] = widget.value
+                continue
+
+            if isinstance(widget, ptg.Container):
+                label, field = iter(widget)
+                OUTPUT[label.value] = field.value
+
+        manager.stop()
+
+
+CONFIG = """
+config:
+    InputField:
+        styles:
+            prompt: dim italic
+            cursor: '@72'
+    Label:
+        styles:
+            value: dim bold
+
+    Window:
+        styles:
+            border: '60'
+            corner: '60'
+
+    Container:
+        styles:
+            border: '96'
+            corner: '96'
+"""
+
+with ptg.YamlLoader() as loader:
+    loader.load(CONFIG)
+
+EditorWidget()
+
+pprint(OUTPUT)
